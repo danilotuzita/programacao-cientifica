4e4ea26a2959c8526515fe416276dd382ca994b7
Danilo Sanchez Tuzita <danilo_st@hotmail.com>
2019-08-15 23:12:40 -0300

    Aula 4 - Final
    * Main:
        -  tradução para o inglês
        -  pequenas correções
        - build final da solução
    * Input:
        - input que deve ser inserido no exe para solução dos problemas pedidos em aula

--- Aula4/Aula4/main.cpp
+++ Aula4/Aula4/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 typedef double(*function)(double, Vector<double>);
 typedef bool(*condition)(double, double);
 
-void printPolinomio(Vector<double> params)
+void printPolynomial(Vector<double> params)
 {
 	int degree = params.size() - 1;
 	double ret = 0;
@@ -18,20 +18,23 @@ void printPolinomio(Vector<double> params)
 		if (p == 0)
 			continue;
 
-		if (i != degree)
+		if (i != degree || p < 0)
 			cout << c[(p > 0)] << " ";
 
 		p = abs(p);
-
 		if (i)
-			cout << p << "x^" << i << " ";
+		{
+			if (p != 1) cout << p;
+			cout << "x^" << i << " ";
+		}
 		else
 			cout << p;
 	}
 	cout << endl;
 }
 
-double polinomio(double x, Vector<double> params)
+// Function that calculates the f(x) of any polynomial
+double polynomimal(double x, Vector<double> params) // params: vetor que guarda o multiplicador de cada grau do polinomio
 {
 	int degree = params.size() - 1;
 	double ret = 0;
@@ -41,92 +44,95 @@ double polinomio(double x, Vector<double> params)
 	return ret;
 }
 
-bool myCondition(double x, double f)
-{
-	return (f > 0.1) || (f < -0.1);
-}
+bool gradientDescent(double f, double min) { return (f > min) || (f <= -min); }
 
-double derivative(function fnc, Vector<double> p, double x, double dt=0.0001)
-{
-	return (fnc(x, p) - fnc(x - dt, p)) / dt;
-}
+double derivative(function fnc, Vector<double> p, double x, double dt=0.000001) { return (fnc(x, p) - fnc(x - dt, p)) / dt; }
 
-double newton(function fnc, Vector<double> p, condition cnd, double beta=0.1, double x=100, bool debug=false)
+double newtonOptimization(function fnc, Vector<double> p, condition cnd, double beta=0.1, double x=100, int max_it=1000, bool debug=false)
 {
+	int it = 0;
 	double f = fnc(x, p);
 	double s = derivative(fnc, p, x);
+	if (debug)	printf( "Parameters:\n\tbeta: %lf\n\tminimal diference: %lf\nf(%8lf) = %8lf ", beta, 0.1, x, f);
 	
-	printPolinomio(p);
-	if(debug) cout << "f(" << x << ") = " << f << endl;
-	
-	while (cnd(f, s))
+	while (cnd(s, 0.0001) && it++ < max_it)
 	{
-		if (debug) cout << "\tnao passou na condicao\n";
 		s = derivative(fnc, p, x);
 		x -= beta * s;
-		if (debug) cout << "\tnovo x = " << x << endl;
+		if (debug) printf("| f'(x) = %8lf | failed the condition\n", s);
 		f = fnc(x, p);
-		if (debug) cout << "f(" << x << ") = " << f << endl;
+		if (debug) printf("f(%8lf) = %8lf ", x, f);
 	}
 
+	if (debug) printf("Iterations: %d out of %d\n", it, max_it);
+	// printf("%lf %d %d\n", beta, it - 1, max_it);
 	return x;
 }
 
 int main()
 {
+	bool cont0 = true;
 	bool cont = false;
 	Vector<double> p;
 	
-	int degree;
-	while (!cont)
+	while (cont0)
 	{
-		p.clear();
-		cout << "Qual o grau da funcao? ";
-		cin >> degree;
-		for (int i = 0; i < degree + 1; i++)
+		int degree;
+		while (!cont)
 		{
-			double temp = 0;
-			cout << (char)('a' + i) << ": ";
-			cin >> temp;
-			p << temp;
+			p.clear();
+			cout << "Polynomial degree: ";
+			cin >> degree;
+			for (int i = degree; i >= 0; i--)
+			{
+				double temp = 0;
+				cout << (char)('a' + i) << ": ";
+				cin >> temp;
+				p << temp;
+			}
+			printPolynomial(p);
+			cout << "Is the polynomial correct? ";
+			cin >> cont;
 		}
-		printPolinomio(p);
-		cout << "A funcao esta correta? ";
-		cin >> cont;
+
+		p.shrink_to_fit();
+
+		/*cont = true;
+		while (cont)
+		{
+			double in = 0;
+			cout << "Digite um x: ";
+			cin >> in;
+			cout << "f(" << in << ") = " << polinomio(in, p) << endl;
+			cout << "Tentar outro x? ";
+			cin >> cont;
+		}*/
+
+		cont = true;
+		while (cont)
+		{
+			double in = 0;
+			cout << " ===== GRADIENT DESCENT ===== \n";
+			cout << "Starting x: ";
+			cin >> in;
+			double r = newtonOptimization(polynomimal, p, gradientDescent, 0.1, in, 1000, true);
+			cout << "Nearest minimum: x = " << r << endl;
+			cout << "Try another starting x? ";
+			cin >> cont;
+		}
+
+		cout << "\n\nTry another polynomial? ";
+		cin >> cont0;
 	}
 
-	p.shrink_to_fit();
-
-	cont = true;
-	while (cont)
-	{
-		double in = 0;
-		cout << "Digite um x: ";
-		cin >> in;
-		cout << "f(" << in << ") = " << polinomio(in, p) << endl;
-		cout << "Tentar outro x? ";
-		cin >> cont;
-	}
-
-	cont = true;
-	while (cont)
-	{
-		double in = 0;
-		cout << "Digite um x: ";
-		cin >> in;
-		double r = newton(polinomio, p, myCondition, 0.01, in);
-		cout << "Vale mais proximo: " << r << endl;
-		cout << "Tentar outro x? ";
-		cin >> cont;
-	}
-
+	system("PAUSE");
 	return 0;
 }
 
 
 /*
-Use o método de Descida de Gradiente para encontrar o mínimo das seguintes funções:
-a) f(x) = x2, sendo x0 = 2.
-b) f(x) = x3 – 2x2 +2, sendo x0 = 2.
-Verifique o que acontece mudando a taxa de aprendizado de 0,1 até 1,0
+Use o metodo de Descida de Gradiente para encontrar o minimo das seguintes funcoes:
+a) f(x) = x^2, sendo x_0 = 2.
+b) f(x) = x^3 - 2x^2 + 2, sendo x_0 = 2.
+Verifique o que acontece mudando a taxa de aprendizado de 0,1 ate 1,0
 */
